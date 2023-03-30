a838e3def7f5651f711fbd594e9ce04503c8f939
Nathan Dyer <nathandyer@fastmail.com>
2023-03-30 14:53:29 -0400

    Add a dark theme (same as docs.securedrop.org)

--- docs/conf.py
+++ docs/conf.py
@@ -128,6 +128,9 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'rtd_dark.css',
+]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
